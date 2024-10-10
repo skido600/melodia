d85becbddd5997d728586a62f02ded26e8e67d60
Chuks <63956312+skido600@users.noreply.github.com>
2024-10-11 01:20:14 +0200

    first commit

--- src/components/Context/ToggleContext.jsx
+++ src/components/Context/ToggleContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 
 export const ToggleContext = createContext();
 
@@ -7,22 +7,10 @@ export const ToggleProvider = ({ children }) => {
   const [toggle, setToggle] = useState(false);
 
   const HandleToggle = () => {
-    setToggle((prev) => !prev); // Toggle state without checking window size
+    if (window.innerWidth < 730) {
+      setToggle(!toggle);
+    }
   };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 730) {
-        setToggle(false); // Close the toggle if window width is greater than or equal to 730
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize); // Clean up the event listener
-    };
-  }, []);
 
   return (
     <ToggleContext.Provider value={{ toggle, HandleToggle }}>
